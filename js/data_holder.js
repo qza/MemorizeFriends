dca64a1e6f8d562d50ea661adac4b2e15ded0716
Zoran <kokovic.zoran@gmail.com>
2011-02-08 22:44:23 +0100

    Holder back in game

---
+++ js/data_holder.js
@@ -0,0 +1,13 @@
+/*
+/ TEST DATA. PUT HERE YOUR OWN DATA.
+*/
+
+// FACEBOOK USER ID
+var facebook_user_id_holder = function(){
+	return "me";
+}
+
+// FACEBOOK API ID
+var facebook_api_id_holder = function(){
+	return "f5dd8ed4fe3f516335ea7d44a03a902f";
+}
