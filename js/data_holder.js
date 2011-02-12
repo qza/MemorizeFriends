5a4a548ada8ffe4beaf226dc9b0dd3be4af78caf
Niksa <unsigned_mind@gmx.net>
2011-02-12 20:30:34 +0100

    FB accessed with FB JS API

--- js/data_holder.js
+++ js/data_holder.js
@@ -9,10 +9,10 @@ var facebook_user_id_holder = function(){
 
 // FACEBOOK APP ID
 var facebook_app_id_holder = function(){
-	return "";
+	return "118130168215562";
 };
 
 // FACEBOOK APP SECRET
 var facebook_app_secret_holder = function(){
-	return "";
+	return "4ab4fdaeaf3818c4504b21d4b2fd189b";
 };
--- js/facebook.js
+++ js/facebook.js
@@ -1,5 +1,12 @@
 var is_test = true;
 
+FB.init({
+	appId : '118130168215562',
+	status : true, // check login status
+	cookie : true, // enable cookies to allow the server to access the session
+	xfbml : true // parse XFBML
+});
+
 get_user_picture_path = function(user_id){
 	return "http://graph.facebook.com/" + user_id + "/picture";
 };
@@ -7,6 +14,21 @@ get_user_picture_path = function(user_id){
 /* Add user friends as card backgrounds */
 add_user_pictures = function(user_id, limit){
 	counter = 0;
+	FB.api('/me', function(me) {
+		var query = FB.Data.query('select title, url, created_time from friendlist  where owner={0}',me.id);
+		query.wait(function(rows) {
+			$.each(rows, function(i,item){
+				counter++;
+				image_path = item.picture;
+				next_id = "img_" + counter;
+				current_css = $("#" + next_id).css(); 
+				$("#" + next_id).css(current_css + "background:url('" + image_path + "')");
+				/* Check card limit */
+  				if(counter==limit) return false;
+  			});
+		});
+		});
+
 	$.getJSON("https://graph.facebook.com/"+user_id+"/friendlists?access_token="+get_token(), {}, 
 	  	function(data) {
 			$.each(data.items, function(i,item){
@@ -28,15 +50,25 @@ var facebook_token_url = function(){
     "&grant_type=client_credentials";
 }
 
-var get_token = function(){
-    $.getJSON(facebook_token_url, {}, 
-	  	function(data) {
-			return data;
-		}
-   );
-};
-
-test_facebook_images = function(limit){
+var test_facebook_images = function(limit){
   user_id = facebook_user_id_holder();
   add_user_pictures(user_id, limit);
 }
+
+var get_token = function(){
+	return "118130168215562|OAwLS1IsVJhR1agus3wgYjSnuo0";
+}
+
+FB.init({
+	appId : '118130168215562',
+	status : true, // check login status
+	cookie : true, // enable cookies to allow the server to access the session
+	xfbml : true // parse XFBML
+	});
+
+var uid = FB.getSession().uid;
+var query = FB.Data.query('select title, url, created_time from link where owner={0}',uid);
+//; 
+
+
+
